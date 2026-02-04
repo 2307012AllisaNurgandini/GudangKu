350af1f2b6cf0da11ee5ca8d982f070327334fc4
2307012AllisaNurgandini <2307012@itg.ac.id>
2026-02-04 16:03:50 +0700

    merubah web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('items.index');
+});
+
+// Login & Logout
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Semua route items harus login
+Route::middleware('auth')->group(function () {
+    Route::resource('items', ItemController::class);
+    Route::get('/print', [ItemController::class, 'print'])->name('items.print');
 });
